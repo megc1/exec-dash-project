9a85b7c075d104afecc735b3effd3043fb6a4120
megc1 <mac492@georgetown.edu>
2019-02-15 13:00:56 -0500

    Import necessary packages/modules
    os module, pandas, plotly and plotly graph objects

--- monthly_sales.py
+++ monthly_sales.py
@@ -1,24 +1,31 @@
 # monthly_sales.py
-
 # TODO: import some modules and/or packages here
+import plotly
+import plotly.graph_objs #referenced https://plot.ly/python/getting-started/#initialization-for-offline-plotting
+import pandas
+import os #referenced Prof. Rossetti's notes on os module (https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/d42b75d4f536ebeca5d6b1934926cdd95aeea714/notes/python/modules/os.md)
+
 
 # TODO: write some Python code here to produce the desired functionality...
 
 print("-----------------------")
-print("MONTH: March 2018")
+print("MONTH: March 2018") #To Do: get actual month/year
+
 
 print("-----------------------")
 print("CRUNCHING THE DATA...")
 
-print("-----------------------")
-print("TOTAL MONTHLY SALES: $12,000.71") #calculate sales
+print("-----------------------") #To Do: calculate sales
+print("TOTAL MONTHLY SALES: $12,000.71") 
 
 
 print("-----------------------")
 print("TOP SELLING PRODUCTS:")
 print("  1) Button-Down Shirt: $6,960.35")
 print("  2) Super Soft Hoodie: $1,875.00")
-print("  3) etc.")
+print("  3) etc.") #To do: retrieve top selling products
 
 print("-----------------------")
 print("VISUALIZING THE DATA...")
+
+#To do: add bar chart of top sellers
