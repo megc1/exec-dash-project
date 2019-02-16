47b2d7673c98ac3c43caa1d97e222ec1f841401c
megc1 <mac492@georgetown.edu>
2019-02-15 23:33:32 -0500

    Deleted commented-out code that hadn't worked (defective for loop and array object) and was replaced, added input validation
    Validates user input using try/except statement

--- monthly_sales.py
+++ monthly_sales.py
@@ -9,20 +9,26 @@ import os #referenced Prof. Rossetti's notes on os module (https://github.com/pr
 
 #User inputs for month and year to get file and import file
 #input function found: https://docs.python.org/3/library/functions.html#input
-get_month = input("Which month's sales data would you like to view? Please enter in MM format. ")
-get_year = input("For which year? Please enter in YYYY format. ")
-year_month = get_year + get_month
-
+#try/except explanation used: https://www.ics.uci.edu/~pattis/ICS-31/lectures/tryexcept/tryexcept.txt
 #try/except to check for valid input values (used https://www.pythonforbeginners.com/error-handling/python-try-and-except)
-#except error type found on Stack Overflow: https://stackoverflow.com/questions/28633555/how-to-handle-filenotfounderror-when-try-except-ioerror-does-not-catch-it
-#Also based on sales-reporting exercise (https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/6d21451ea2d8f992fb067d28ccb37ce37219017d/exercises/sales-reporting/pandas_explore.py)
+#except error type found here (and also somewhat organically, getting that error the first few times that I had that error running my code...): https://docs.python.org/3/library/exceptions.html 
+while True:
+    try:
+        get_month = input("Which month's sales data would you like to view? Please enter in MM format. ")
+        get_year = input("For which year? Please enter in YYYY format. ")
+        year_month = get_year + get_month
 
-#try: 
-CSV_FILENAME = "sales-"+ get_year + get_month+ ".csv"
+     #Also based on sales-reporting exercise (https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/6d21451ea2d8f992fb067d28ccb37ce37219017d/exercises/sales-reporting/pandas_explore.py)
+ 
+        CSV_FILENAME = "sales-"+ get_year + get_month+ ".csv"
    
-CSV_FILEPATH = os.path.join(os.path.dirname(__file__), "data", CSV_FILENAME)
+        CSV_FILEPATH = os.path.join(os.path.dirname(__file__), "data", CSV_FILENAME)
     
-df = pd.read_csv(CSV_FILEPATH)
+        df = pd.read_csv(CSV_FILEPATH)
+    except FileNotFoundError:
+        print("Oops! There doesn't seem to be a file matching that name. Please check that your file is named in proper format and in a folder named data and try again!")
+    else:
+        break
 
 #adapted from sales-reporting exercise
 def month_lookup(month):
@@ -56,23 +62,13 @@ print("TOTAL MONTHLY SALES: "+ "${0:,.2f}".format(SumSales))
 print("-----------------------")
 
 print("TOP SELLING PRODUCTS:")
-
+# #Also consulted: http://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.iterrows.html
+# #Also consulted: https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas
 #Referenced Stack Overflow to get length of object: https://stackoverflow.com/questions/518021/is-arr-len-the-preferred-way-to-get-the-length-of-an-array-in-python
 #Iloc and print output adapted from sales-reporting exercise (https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/6d21451ea2d8f992fb067d28ccb37ce37219017d/exercises/sales-reporting/pandas_explore.py)
 total_count = len(prodsum_sorted)
 for i in range(total_count):
     print(str(i+1)+') '+str(prodsum_sorted.iloc[i][0])+" "+"${0:,.2f}".format(prodsum_sorted.iloc[i][1]))
-# most_sales = []
-# ranking = 1 #counter variable
-# #Referenced: exec dash starter code from Prof. Rossetti to get iterrows function and adapted for loop (https://github.com/s2t2/exec-dash-starter-py/blob/master/monthly_sales.py)
-# #Also consulted: http://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.iterrows.html
-# #Also consulted: https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas
-# for i, row in prodsum_sorted.iterrows():
-#     p = {"rank": ranking, "name": row.name, "monthly_sales": row["sales price"]}
-#     most_sales.append(p)
-#     ranking = ranking + 1
-# for p in most_sales:
-#     print(" " + str(p["rank"]) + ") " + str(p["name"]) + ": " + "${0:,.2f}".format(p["monthly_sales"]))
 
 print("-----------------------")
 
@@ -80,13 +76,12 @@ print("VISUALIZING THE DATA...")
 #Referenced: https://plot.ly/python/horizontal-bar-charts/
 
 
+#tolist() function explanation used: https://stackoverflow.com/questions/23748995/pandas-dataframe-to-list
+#tolist() syntax adapted from example on Geeks for Geeks: https://www.geeksforgeeks.org/python-pandas-series-tolist/
 product_names_list=prodsum_sorted['product'].tolist()
 product_sales_sorted = prodsum_sorted['sales price']
 bar_labels = ['${:,.2f}'.format(p) for p in product_sales_sorted]
 
-# product_sales_sorted = [p["monthly_sales"] for p in most_sales]
-# product_names_list = [p["name"] for p in most_sales]
-# bar_labels = ['${:,.2f}'.format(p["monthly_sales"]) for p in most_sales]
 
 #Referenced: https://plot.ly/python/getting-started/#initialization-for-offline-plotting
 #Referenced: https://plot.ly/python/user-guide/
@@ -113,14 +108,4 @@ py.offline.plot({
    # "layout": go.Layout(title="hello world")
 # }, auto_open=True)
 
-#TODO: print bar chart
 
-#TODO: edit formatting
-
-  
-
-
-
-
-#except OSError:
-#print("The file you're looking for doesn't seem to be there. Please check that a correctly titled file exists in the data folder and try again!")
