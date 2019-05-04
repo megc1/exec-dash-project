b5117a976912b8ff3443a789e1e2bf6e99a1c5db
megc1 <mac492@georgetown.edu>
2019-05-04 18:59:18 -0400

    get_top_sellers testing

--- test/exec_dash_test.py
+++ test/exec_dash_test.py
@@ -1,7 +1,8 @@
 import os
 import pytest
+import pandas as pd
 
-from app.monthly_sales import to_usd
+from app.monthly_sales import to_usd, get_top_sellers
 #Reference: Prof. Rossetti's example solution
 
 def test_to_usd():
@@ -13,3 +14,19 @@ def test_to_usd():
     assert to_usd(2.148) == "$2.15"
     #test thousands separators
     assert to_usd(222333.456) == "$222,333.46"
+
+def test_get_top_sellers():
+    CSV_FILENAME = "sales-201904.csv"
+    CSV_FILEPATH = os.path.join(os.path.dirname(__file__), "test_data", CSV_FILENAME)
+    df = pd.read_csv(CSV_FILEPATH)
+    results = get_top_sellers(df)
+    expected_result = [
+        {'name': 'Button-Down Shirt', 'monthly sales': 5008.849999999999},
+        {'name': 'Super Soft Hoodie', 'monthly sales': 1800.0},
+        {'name': 'Khaki Pants', 'monthly sales': 1691.0},
+        {'name': 'Vintage Logo Tee', 'monthly sales': 877.25},
+        {'name': 'Brown Boots', 'monthly sales': 250.0},
+        {'name': 'Sticker Pack', 'monthly sales': 220.5},
+        {'name': 'Baseball Cap', 'monthly sales': 156.31}
+    ]
+    assert results == expected_result
